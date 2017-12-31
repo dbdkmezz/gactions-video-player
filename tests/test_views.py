50350cad6f76d28c63b8a4e5936fadb824749ef2
Paul Carver <dbdkmezz@gmail.com>
2017-12-31 17:02:13 +0000

    fixies

--- tests/test_views.py
+++ tests/test_views.py
@@ -16,21 +16,29 @@ class TestViews(TestCase):
         self.assertIn("Hello world", str(result.content))
 
     def test_asks_what_would_you_like_to_play(self):
-        result = index(MockRequest(argument_text_value=None))
+        result = index(MockRequest(text=''))
         self.assertIs(type(result), JsonResponse)
         self.assertEqual("What would you like to play?",
                          GoogleUtils.get_text_from_google_response(result))
 
     def test_says_i_dont_know_how(self):
-        result = index(MockRequest(argument_text_value='dance'))
+        result = index(MockRequest(text='dance'))
         self.assertIs(type(result), JsonResponse)
         self.assertIn("I don't know how to dance",
                       GoogleUtils.get_text_from_google_response(result))
 
     def test_tell_response_for_blue_planet(self):
         with patch('apps.video_player.views.Messenger') as messenger:
-            result = index(MockRequest(argument_text_value='Blue Planet'))
+            result = index(MockRequest(text='Blue Planet'))
         self.assertEqual(messenger.open_website.call_count, 1)
         self.assertIn(
             "Opening Blue Planet",
+            GoogleUtils.get_text_from_google_response(result))
+
+    def test_pause(self):
+        with patch('apps.video_player.views.Messenger') as messenger:
+            result = index(MockRequest(text='play'))
+        self.assertEqual(messenger.play_pause_video.call_count, 1)
+        self.assertEqual(
+            "On it!",
             GoogleUtils.get_text_from_google_response(result))
